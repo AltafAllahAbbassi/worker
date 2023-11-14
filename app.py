43b1cee6c9cd8c2761abe7db93208d4908f353e6
altaf <altafallahabbassi@gmail.com>
2023-11-14 18:44:08 -0500

    fix port in docker file

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import json
 from flask import Flask, jsonify
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 import torch 
--- dockerfile
+++ dockerfile
@@ -6,7 +6,7 @@ COPY . /app
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 80
+EXPOSE 5000
 
 ENV FLASK_APP=app.py
 ENV FLASK_RUN_HOST=0.0.0.0
